caa075b27565cb2b95640018df61697ff785273c
amohata007 <amohata.007@gmail.com>
2024-08-07 18:53:11 +0530

    Laying the foundation - 3

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Hello world in React
-// const heading = React.createElement("h1",{},"Hello World from React");
-// const root = ReactDOM.createRoot(document.getElementById("myText"));
-// root.render(heading);
 
-//Nested Rendering
-const parent = React.createElement("div",{id: "parent"}, 
-React.createElement("div",{id: "child"},
-[React.createElement("h1",{},"Hi I'm H1 Tag."),
-React.createElement("h2",{},"Hi I'm H2 Tag.")]));
-
+const parent = <h1>Hello World from JSX..!!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("myText"));
 root.render(parent);
+
+//Functional Component
+//Multiple ways to include other components(Composite Components).
+const data = 1000;
+const HeadingComponent = () => {
+  return (
+    <div>
+      <HeadingComponent2 />
+      <HeadingComponent2></HeadingComponent2>
+      {HeadingComponent2()}
+      {data}
+      <h1>This is Functional Component.</h1>
+    </div>
+  );
+};
+const funcRoot = ReactDOM.createRoot(document.getElementById("myText2"));
+funcRoot.render(<HeadingComponent />);
+
+//Component Composite
+const HeadingComponent2 = () => {
+  return (
+    <div>
+      <h2>I'm Component Composite.</h2>
+    </div>
+  );
+};
